d5b1b2074560271a97c5d630486b6d7bb5fbc422
syooool <79139053+syooool@users.noreply.github.com>
2021-06-04 09:56:37 +0800

    123

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const app = document.getElementById('root');
 
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'http://cfc99fb76966.ngrok.io/msg', true)
 
 request.onload = function () {
   const text = document.getElementById('your-text');
   console.log('Hello World');
 
   var data = this.response;
-  var jsonData = JSON.parse(this.response);
+  //var jsonData = JSON.parse(this.response);
 
   console.log('This is your data', data);
-  console.log('This is your data read by Json', jsonData);
+  //console.log('This is your data read by Json', jsonData);
+  
 
   if (request.status >= 200 && request.status < 400) {
     // change text here
-    text.innerHTML = data;
+    text.innerHTML =data;
   } else {
     text.innerHTML = "Error";
   }
